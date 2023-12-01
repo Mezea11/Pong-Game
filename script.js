7d8a79d9594149f33e5c4f0de4ae6d925f37e3d5
PelleHanspers <p.hanspers@gmail.com>
2023-12-01 14:33:50 +0100

    lagt till nya sorts hinder

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // Get the canvas element and its context
 const canvas = document.getElementById('pongCanvas');
 const ctx = canvas.getContext('2d');
@@ -19,6 +21,7 @@ let laser;
 let laserArray = [];
 
 let obstacleArray = [];
+let obstacleTwoArray = [];
 
 let score = 0;
 
@@ -48,6 +51,8 @@ const rightPaddle = {
 // Create the ball
 const ball = { x: canvas.width / 2, y: canvas.height / 2, radius: 8, speedX: 5, speedY: 5 };
 
+getRandomNumber();
+
 // generate random number
 function getRandomNumber(min, max) {
   min = Math.ceil(min);
@@ -58,23 +63,71 @@ function getRandomNumber(min, max) {
 // skapa hinder
 function CreateObstacle() {
   if (score === 200 && obstacleArray.length == 0) {
+    let obstacleX = getRandomNumber(100, 300);
+    let obstacleY = 50;
+    // hinder bestående av 4 stora block
+    let makeObstacle = (x, y) => ({
+      x: x,
+      y: y,
+      width: 20,
+      height: 20,
+      status: 1,
+      speed: 0,
+    });
+
+    obstacleArray.push(makeObstacle(obstacleX, obstacleY));
+    obstacleArray.push(makeObstacle(obstacleX + 20, obstacleY));
+    obstacleArray.push(makeObstacle(obstacleX, obstacleY + 20));
+    obstacleArray.push(makeObstacle(obstacleX + 20, obstacleY + 20));
+  }
+  if (score === 400 && obstacleArray.length <= 4) {
     let obstacleX = getRandomNumber(100, 200);
     let obstacleY = obstacleX;
+    // hinder bestående av 9 mindre block
+    let makeObstacle = (x, y) => ({
+      x: x,
+      y: y,
+      width: 10,
+      height: 10,
+      status: 1,
+      speed: 3,
+    });
 
-      let makeObstacle = (x, y) => ({
-        x: x,
-        y: y,
-        width: 20,
-        height: 20,
-        status: 1,
-      });
-
-      obstacleArray.push(makeObstacle (obstacleX, obstacleY));
-      obstacleArray.push(makeObstacle (obstacleX + 20, obstacleY));
-      obstacleArray.push(makeObstacle (obstacleX, obstacleY + 20));
-      obstacleArray.push(makeObstacle (obstacleX + 20, obstacleY + 20));
-    }
+    obstacleArray.push(makeObstacle(obstacleX, obstacleY));
+    obstacleArray.push(makeObstacle(obstacleX + 10, obstacleY));
+    obstacleArray.push(makeObstacle(obstacleX, obstacleY + 10));
+    obstacleArray.push(makeObstacle(obstacleX + 10, obstacleY + 10));
+    obstacleArray.push(makeObstacle(obstacleX + 20, obstacleY));
+    obstacleArray.push(makeObstacle(obstacleX, obstacleY + 20));
+    obstacleArray.push(makeObstacle(obstacleX + 10, obstacleY + 20));
+    obstacleArray.push(makeObstacle(obstacleX + 20, obstacleY + 10));
+    obstacleArray.push(makeObstacle(obstacleX + 20, obstacleY + 20));
   }
+  if (score === 500 && obstacleTwoArray.length <= 4) {
+    let obstacleX = getRandomNumber(100, 200);
+    let obstacleY = obstacleX;
+    // hinder bestående av 9 mindre block
+    let makeObstacle = (x, y) => ({
+      x: x,
+      y: y,
+      width: 10,
+      height: 10,
+      status: 1,
+      speed: 3,
+      hit: false,
+    });
+
+    obstacleTwoArray.push(makeObstacle(obstacleX, obstacleY));
+    obstacleTwoArray.push(makeObstacle(obstacleX + 10, obstacleY));
+    obstacleTwoArray.push(makeObstacle(obstacleX, obstacleY + 10));
+    obstacleTwoArray.push(makeObstacle(obstacleX + 10, obstacleY + 10));
+    obstacleTwoArray.push(makeObstacle(obstacleX + 20, obstacleY));
+    obstacleTwoArray.push(makeObstacle(obstacleX, obstacleY + 20));
+    obstacleTwoArray.push(makeObstacle(obstacleX + 10, obstacleY + 20));
+    obstacleTwoArray.push(makeObstacle(obstacleX + 20, obstacleY + 10));
+    obstacleTwoArray.push(makeObstacle(obstacleX + 20, obstacleY + 20));
+  }
+}
 
 // Event listeners för att hantera spelarens rörelse + laser
 window.addEventListener("keydown", (event) => {
@@ -99,7 +152,7 @@ window.addEventListener("keyup", (event) => {
     // audio will trigger everytime you push space by resetting audio
     laserSound.currentTime = 0;
     laserSound.play();
-   }
+  }
 });
 
 // move function for left side (player controlled)
@@ -144,15 +197,25 @@ function draw() {
   ctx.fill();
   ctx.closePath();
 
-  // draw obsactle
-  for (i = 0; i < obstacleArray.length; i++) {
+  // draw obstactle
+  for (let i = 0; i < obstacleArray.length; i++) {
     let obstacle = obstacleArray[i];
     if (obstacle.status === 1) {
-    ctx.fillStyle = 'grey';
-    ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+      ctx.fillStyle = 'pink';
+      ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
     }
   }
-
+  // draw obstacleTwo
+  for (let i = 0; i < obstacleTwoArray.length; i++) {
+    let obstacle = obstacleTwoArray[i];
+    if (!obstacle.hit && obstacle.status === 1) {
+      ctx.fillStyle = 'brown';
+      ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+    } else if (obstacle.hit && obstacle.status === 1) {
+      ctx.fillStyle = 'red';
+      ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+    }
+  }
   // draw laser
   for (let i = 0; i < laserArray.length; i++) {
     let laser = laserArray[i];
@@ -161,6 +224,7 @@ function draw() {
     ctx.fillRect(laser.x, laser.y, laser.width, laser.height);
   }
 
+  // draw score
   ctx.fillStyle = "white";
   ctx.font = "16px courier";
   ctx.fillText(score, 5, 20);
@@ -171,6 +235,16 @@ function update() {
   // Move the ball
   ball.x += ball.speedX;
   ball.y += ball.speedY;
+
+  // movement hinder
+  for (let i = 0; i < obstacleArray.length; i++) {
+    let obstacle = obstacleArray[i];
+    obstacle.y += obstacle.speed;
+    // hinder bounce off top and bottom edges    
+    if (obstacle.y - obstacle.height < 0 || obstacle.y + obstacle.height > canvas.height) {
+      obstacle.speed = -obstacle.speed;
+    }
+  }
 
   // Bounce off the top and bottom edges
   if (ball.y - ball.radius < 0 || ball.y + ball.radius > canvas.height) {
@@ -186,26 +260,53 @@ function update() {
     paddleBall.play();
     // right paddle stop moving after ball hits paddle
     rightPaddle.hit = false;
-  } 
+  }
   // make right paddle move if ball leaves canvas
   if (ball.x + ball.radius > canvas.width) {
     rightPaddle.hit = true;
   }
-  // ball and object collision
+  // ball and obstacle collision
   for (let i = 0; i < obstacleArray.length; i++) {
     let obstacle = obstacleArray[i];
-      if (
-        (ball.x + ball.radius > obstacle.x && 
+    if (
+      (ball.x + ball.radius > obstacle.x &&
         ball.x - ball.radius < obstacle.x + obstacle.width &&
         ball.y > obstacle.y &&
-        ball.y < obstacle.y + obstacle.height) 
-      ) {
-        ball.speedX = -ball.speedX;
-        rightPaddle.hit = true;
-        obstacle.status = 0;
-        obstacleArray.splice(i, 1);
-        i--;
-        obstacleBall.play();
+        ball.y < obstacle.y + obstacle.height)
+    ) {
+      ball.speedX = -ball.speedX;
+      rightPaddle.hit = true;
+      obstacle.status = 0;
+      obstacleArray.splice(i, 1);
+      i--;
+      obstacleBall.play();
+    }
+  }
+  // ball and obstacleTwo collision
+  for (let i = 0; i < obstacleTwoArray.length; i++) {
+    let obstacle = obstacleTwoArray[i];
+    if (
+      (!obstacle.hit && ball.x + ball.radius > obstacle.x &&
+        ball.x - ball.radius < obstacle.x + obstacle.width &&
+        ball.y > obstacle.y &&
+        ball.y < obstacle.y + obstacle.height)
+    ) {
+      obstacle.hit = true;
+      ball.speedX = -ball.speedX;
+      rightPaddle.hit = true;
+      obstacleBall.play();
+    } else if (
+      (obstacle.hit && ball.x + ball.radius > obstacle.x &&
+        ball.x - ball.radius < obstacle.x + obstacle.width &&
+        ball.y > obstacle.y &&
+        ball.y < obstacle.y + obstacle.height)
+    ) {
+      ball.speedX = -ball.speedX;
+      rightPaddle.hit = true;
+      obstacle.status = 0;
+      obstacleTwoArray.splice(i, 1);
+      i--;
+      obstacleBall.play();
     }
   }
   // check for and handle collision between laser and ball, and handle laserArray when laser leaves canvas
@@ -229,16 +330,17 @@ function update() {
       laserArray.splice(i, 1);
     }
   }
-  
+
   // Check for scoring
   if (ball.x - ball.radius < 0 || ball.x + ball.radius > canvas.width) {
     // Reset ball position
     score += 100;
     ball.x = canvas.width / 2;
     ball.y = canvas.height / 2;
-  }    
+  }
 }
 
+// skapar laser-skott
 function shoot() {
   let laser = {
     x: leftPaddle.x / 2,
@@ -246,16 +348,16 @@ function shoot() {
     width: 20,
     height: 20,
     speed: 12,
-//    used: false,
+    //    used: false,
   };
-  laserArray.push(laser); 
+  laserArray.push(laser);
 }
 
 // Game loop
-function gameLoop() { 
+function gameLoop() {
   moveLeftPaddle();
   moveRightpaddle();
-  CreateObstacle(); 
+  CreateObstacle();
   draw();
   update();
   requestAnimationFrame(gameLoop);
