4cd88456f12adb98ba976937bc3ed80b035c9085
PelleHanspers <p.hanspers@gmail.com>
2023-12-07 14:03:29 +0100

    fixed module commit

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ canvas.height = 300;
 
 // menu
 
+getRandomNumber();
+
+// generate random number
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 const menu = document.getElementById("menu");
 const instructionsDiv = document.getElementById("instructions");
 const difficultyDiv = document.getElementById("menu");
@@ -99,48 +108,6 @@ const ball = {
   speedX: 5,
   speedY: 5
 };
-
-// levels & powerups 
-
-function getLevel() { 
-  if (score <= 200) { 
-    lvlcount = 2; 
-    rightPaddle.speed = 3; 
-
-    if (score === 200 && powerUpArray.length == 0) {
-      let powerUpX = getRandomNumber(100, 500);
-      let powerUpY = getRandomNumber(20, 280);
-      let makepowerUp = (x, y) => ({
-        x: x,
-        y: y,
-        width: 20,
-        height: 20,
-        status: 1,
-        speed: 0,
-      })
-
-      powerUpArray.push(makepowerUp(powerUpX, powerUpY));
-    }
-  }
-  }
-
-  if (score >= 400) { 
-    lvlcount = 3; 
-    rightPaddle.speed = 7; 
-
-  if (powerUpArray.length >= 0 && powerUpArray.length <= 0) {
-    let powerUpX = getRandomNumber(100, 500);
-    let powerUpY = getRandomNumber(20, 280);
-    let makepowerUp = (x, y) => ({
-      x: x,
-      y: y,
-      width: 20,
-      height: 20,
-      status: 1,
-      speed: 2,
-    })
-
-    powerUpArray.push(makepowerUp(powerUpX, powerUpY));
 
 
 
